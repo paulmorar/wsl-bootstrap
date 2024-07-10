9d071536530acdf680a051894b061e283b9abd21
Paul Morar <paulcornelmorar@gmail.com>
2024-07-10 22:50:35 +0200

    Fix typo

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,71 +1,63 @@
 #!/usr/bin/env bash
 
-EXTRA_APT_PACKAGES_TO_INSTALL="tree stow, build-essential, zip, unzip"
+EXTRA_APT_PACKAGES_TO_INSTALL="tree, stow, build-essential, zip, unzip"
 
-install_extras()
-{
+install_extras() {
 	sudo apt-get update
 	sudo apt-get upgrade -y
 	sudo apt-get install -y \
 		${EXTRA_APT_PACKAGES_TO_INSTALL}
 }
 
-install_updates()
-{
+install_updates() {
 	sudo apt-get update
 	sudo apt-get upgrade -y
 }
 
-update_git()
-{
+update_git() {
 	sudo add-apt-repository -y ppa:git-core/ppa
-    sudo apt-get update
-    sudo apt-get install git -y
+	sudo apt-get update
+	sudo apt-get install git -y
 }
 
-install_node()
-{
+install_node() {
 	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-    source ~/.bashrc
+	source ~/.bashrc
 	nvm install node
-    echo "Running Node"
-    node -v
-    echo "Running NPM"
-    npm -v
+	echo "Running Node"
+	node -v
+	echo "Running NPM"
+	npm -v
 }
 
-install_go()
-{
+install_go() {
 	wget https://dl.google.com/go/go1.22.0.linux-amd64.tar.gz
-    sudo tar -C /usr/local -xzf go1.22.0.linux-amd64.tar.gz
-    echo "Installed GO successfully"
-    go version
+	sudo tar -C /usr/local -xzf go1.22.0.linux-amd64.tar.gz
+	echo "Installed GO successfully"
+	go version
 }
 
-install_fzf()
-{
+install_fzf() {
 	git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 	~/.fzf/install
 }
 
-install_zsh()
-{
+install_zsh() {
 	sudo apt install zsh -y
-    zsh --version
-    # https://ohmyz.sh/
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    # https://github.com/romkatv/powerlevel10k
-    git clone https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
-    # https://github.com/zsh-users/zsh-autosuggestions
-    git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-    # https://github.com/zsh-users/zsh-syntax-highlighting
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-    # https://github.com/Pilaton/OhMyZsh-full-autoupdate
-    git clone https://github.com/Pilaton/OhMyZsh-full-autoupdate.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/ohmyzsh-full-autoupdate
+	zsh --version
+	# https://ohmyz.sh/
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	# https://github.com/romkatv/powerlevel10k
+	git clone https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+	# https://github.com/zsh-users/zsh-autosuggestions
+	git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+	# https://github.com/zsh-users/zsh-syntax-highlighting
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+	# https://github.com/Pilaton/OhMyZsh-full-autoupdate
+	git clone https://github.com/Pilaton/OhMyZsh-full-autoupdate.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/ohmyzsh-full-autoupdate
 }
 
-main()
-{
+main() {
 	echo "Updating installed vanilla packages..."
 	install_updates | indent
 	echo "Updating git"
